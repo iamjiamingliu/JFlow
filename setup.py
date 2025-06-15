b84035ff7f1690a610b297ee44557b1f7a215284
iamjiamingliu <57842784+iamjiamingliu@users.noreply.github.com>
2025-06-14 19:30:54 -0700

    added setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='JFlow',
+    version='0.1',
+    description="An embedded workflow orchestration framework that's Pythonic, async native, and lightweight",
+    author='Jiaming Liu',
+    author_email='jiamingliu@ucsb.edu',
+    packages=find_packages(),
+    zip_safe=False
+)
